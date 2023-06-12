8c718fea44d1112961cda61e6795e17c7985e142
Jeff B <amlvt225@icloud.com>
2023-06-12 10:18:18 -0400

    Adding a second option for cases

--- frontend/integral-data/src/CaseStudiesCarousel.js
+++ frontend/integral-data/src/CaseStudiesCarousel.js
@@ -1,30 +1,18 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { Box, Container, IconButton, Typography } from '@mui/material';
 import Slider from 'react-slick';
 import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 import { caseStudiesData } from './data/caseStudiesData';
 import { CaseStudy } from './components/CaseStudy';
 
-// Custom arrow component for Carousel
-const Arrow = (props) => {
-  const { className, style, onClick, direction } = props;
-  return (
-    <IconButton
-      className={className}
-      style={{ ...style, position: 'absolute', top: '50%', transform: 'translateY(-50%)' }}
-      onClick={onClick}
-    >
-      {direction === 'left' ? <ArrowBackIos /> : <ArrowForwardIos />}
-    </IconButton>
-  );
-};
-
-
 const CaseStudyPageCarousel = () => {
   const { id } = useParams();
   const initialSlide = caseStudiesData.findIndex((study) => study.id === parseInt(id));
   const [currentSlide, setCurrentSlide] = useState(initialSlide);
+
+  const prevCase = caseStudiesData[(currentSlide - 1 + caseStudiesData.length) % caseStudiesData.length];
+  const nextCase = caseStudiesData[(currentSlide + 1) % caseStudiesData.length];
 
   const settings = {
     dots: false,
@@ -36,17 +24,13 @@ const CaseStudyPageCarousel = () => {
     afterChange: current => setCurrentSlide(current),
     nextArrow: (
       <Box display="flex" flexDirection="column" alignItems="center" justifyContent="space-between">
-        {/* <Typography variant="caption">{caseStudiesData[(currentSlide + 1) % caseStudiesData.length].title}</Typography> */}
-        <Typography variant="caption" color="white">{caseStudiesData[(currentSlide + 1) % caseStudiesData.length].title}</Typography>
-
-        {/* <IconButton><ArrowForwardIos /></IconButton> */}
+        <Typography variant="caption">{nextCase && nextCase.title}</Typography>
+        
       </Box>
     ),
     prevArrow: (
       <Box display="flex" flexDirection="column" alignItems="center" justifyContent="space-between">
-        {/* <Typography variant="caption">{caseStudiesData[(currentSlide - 1 + caseStudiesData.length) % caseStudiesData.length].title}</Typography> */}
-        <Typography variant="caption" color="white">{caseStudiesData[(currentSlide - 1) % caseStudiesData.length].title}</Typography>
-        {/* <IconButton><ArrowBackIos /></IconButton> */}
+        <Typography variant="caption">{prevCase && prevCase.title}</Typography>
       </Box>
     ),
     responsive: [
@@ -61,6 +45,20 @@ const CaseStudyPageCarousel = () => {
 
   return (
     <Container sx={{ py: '40px' }}>
+      <Box display="flex" justifyContent="space-between">
+        {prevCase && 
+          <Link to={`/case_study/${prevCase.id}`} style={{textDecoration: 'none'}}>
+            <ArrowBackIos />
+            <Typography variant="caption" style={{ color: 'white' }}>{prevCase.title}</Typography>
+
+          </Link>}
+        {nextCase && 
+          <Link to={`/case_study/${nextCase.id}`} style={{textDecoration: 'none'}}>
+            <Typography variant="caption" style={{ color: 'white' }}>{nextCase.title}</Typography>
+            <ArrowForwardIos />
+
+          </Link>}
+      </Box>
       <Slider {...settings}>
         {caseStudiesData.map((study) => (
           <div key={study.id}>
---
+++ frontend/integral-data/src/CaseStudySingle.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { Box, Container, Typography, IconButton } from '@mui/material';
+import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
+import { caseStudiesData } from './data/caseStudiesData';
+import { CaseStudy } from './components/CaseStudy';
+
+const CaseStudyPage = () => {
+  const { id } = useParams();
+  const currentId = parseInt(id);
+  const currentCaseStudy = caseStudiesData.find((study) => study.id === currentId);
+
+  const prevCase = caseStudiesData.find((study) => study.id === (currentId - 1));
+  const nextCase = caseStudiesData.find((study) => study.id === (currentId + 1));
+
+  return (
+    <Container sx={{ py: '40px' }}>
+      {currentCaseStudy && <CaseStudy {...currentCaseStudy} />}
+      <Box display="flex" justifyContent="space-between" alignItems="center" mt={1}>
+        {prevCase ? 
+          <Link to={`/case_study_v2/${prevCase.id}`} style={{textDecoration: 'none', display: 'flex', alignItems: 'center'}}>
+            <IconButton color="white"><ArrowBackIos /></IconButton>
+            <Typography variant="caption" style={{ color: 'white' }}>Previous: {prevCase.title}</Typography>
+          </Link> : <Box></Box> // empty box to take up space when there is no previous case
+        }
+        {nextCase ? 
+          <Link to={`/case_study_v2/${nextCase.id}`} style={{textDecoration: 'none', display: 'flex', alignItems: 'center'}}>
+            <Typography variant="caption" style={{ color: 'white' }}>Next: {nextCase.title}</Typography>
+            <IconButton color="white"><ArrowForwardIos /></IconButton>
+          </Link> : <Box></Box> // empty box to take up space when there is no next case
+        }
+      </Box>
+    </Container>
+  );
+};
+
+export default CaseStudyPage;
