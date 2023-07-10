b3f3dd5f68ee75f91a9043f84d44261ff5ce0118
Mike Barton <mbarton0831@gmail.com>
2023-07-10 16:15:03 -0400

    adding padding to container page

--- frontend/integral-data/src/CaseStudiesOverviewPage.js
+++ frontend/integral-data/src/CaseStudiesOverviewPage.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Grid, Container } from '@mui/material';
-import CaseStudyCard from './components/CaseStudyCard';
-import { caseStudiesData } from './data/caseStudiesData';
-import TitleSubtitle from './components/TitleSubtitle';
+import React from "react";
+import { Grid, Container } from "@mui/material";
+import CaseStudyCard from "./components/CaseStudyCard";
+import { caseStudiesData } from "./data/caseStudiesData";
+import TitleSubtitle from "./components/TitleSubtitle";
 
 export default function CaseStudiesPage() {
   return (
     <Container>
-       <TitleSubtitle 
-                title="Case Studies" 
-                subtitle="From Problem to Solution: Highlighting Our Client Projects"
-                color="white"
-                  />
+      <TitleSubtitle
+        title="Case Studies"
+        subtitle="From Problem to Solution: Highlighting Our Client Projects"
+        color="white"
+      />
 
-      <Grid container spacing={6}>
-        {caseStudiesData.map(caseStudy => (
+      <Grid container spacing={6} mb={3}>
+        {caseStudiesData.map((caseStudy) => (
           <Grid item xs={12} sm={6} md={4} key={caseStudy.id}>
             <CaseStudyCard study={caseStudy} />
           </Grid>
