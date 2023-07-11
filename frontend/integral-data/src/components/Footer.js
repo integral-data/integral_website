77d1327b3d1639ba46df60e670c9e411592a97d9
Jeff B <amlvt225@icloud.com>
2023-07-11 13:42:20 -0400

    Fixing footer logo

--- frontend/integral-data/src/components/Footer.js
+++ frontend/integral-data/src/components/Footer.js
@@ -8,8 +8,6 @@ import {
 import { IntegralRotatingLogo } from "./RotatingLogo";
 
 const Footer = () => {
-
-
   return (
     <Box
       component="footer"
@@ -22,9 +20,9 @@ const Footer = () => {
           justifyContent="space-between"
           alignItems="center"
           mb={1}
-          height="60px"
+          height="40px"
         >
-          <Grid item md={4}>
+          <Grid item md={8} xs={8}>
             <Typography variant="h5" fontWeight={700}>
               Integral Data
             </Typography>
@@ -33,68 +31,10 @@ const Footer = () => {
               reserved.
             </Typography>
           </Grid>
-          <Grid item md={4} xs={12}></Grid>
-          <IntegralRotatingLogo height="40px" width="40px" />
+          <Grid item md={4} xs={4} display="flex" justifyContent="flex-end">
+            <IntegralRotatingLogo height="40px" width="40px" />
+          </Grid>
         </Grid>
-        {/* <Box display="flex" justifyContent="space-between">
-          <Box>
-            <Typography variant="body1" fontWeight={600}>
-              Company
-            </Typography>
-
-            <Typography variant="body2">
-              <ScrollLink
-                to="contact-section"
-                spy={true}
-                smooth={true}
-                duration={500}
-                offset={-60}
-                color="inherit"
-                underline="none"
-                activeClass="active"
-              >
-                Contact
-              </ScrollLink>
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="body1" fontWeight={600}>
-              Resources
-            </Typography>
-            <Typography variant="body2">
-              <ScrollOrRouteLink
-                to="expertise"
-                color="inherit"
-                underline="none"
-                activeClass="active"
-              >
-                Skills
-              </ScrollOrRouteLink>
-            </Typography>
-
-            <Typography variant="body2">
-              <ScrollOrRouteLink
-                to="testimonials"
-                color="inherit"
-                underline="none"
-                activeClass="active"
-              >
-                Testimonials
-              </ScrollOrRouteLink>
-            </Typography>
-
-            <Typography variant="body2">
-              <ScrollOrRouteLink
-                to="case-studies"
-                color="inherit"
-                underline="none"
-                activeClass="active"
-              >
-                Case Studies
-              </ScrollOrRouteLink>
-            </Typography>
-          </Box>
-        </Box> */}
       </Container>
     </Box>
   );
